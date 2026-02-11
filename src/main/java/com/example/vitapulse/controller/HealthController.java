dbd003024270e73e3bba4902caaaab82a5be21d9
Alexandra <134061613+alexxamsv@users.noreply.github.com>
2026-02-11 14:01:07 +0200

    chore: init

---
+++ src/main/java/com/example/vitapulse/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.vitapulse.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "VitaPulse is alive!";
+    }
+}
---
+++ src/test/java/com/example/vitapulse/VitapulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vitapulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitapulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
